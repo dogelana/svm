8d2435a3008fdd424cb41f2da3c9b1dd41e4cffb
Dogelana (DGLN) <team@dogelana.com>
2024-10-21 04:16:25 -0400

    Update customgithublist.js

--- scripts/customgithublist.js
+++ scripts/customgithublist.js
@@ -21,7 +21,7 @@ function initializeCustomGithubList() {
     container.appendChild(customGithubListArea);
 
     // Fetch the GitHub tree data
-    fetch('https://api.github.com/repos/dogelana/svm/git/trees/d927d4c4967bdcdab138d0171e5aac8c2712670d?recursive=1')
+    fetch('https://api.github.com/repos/dogelana/svm/git/trees/f9d21397ffba7c5e140e54f3a73bfdd575a42a36?recursive=1')
       .then(response => response.json())
       .then(data => {
         const fileList = data.tree.filter(item => item.type === 'blob' && item.path.endsWith('.js'));
